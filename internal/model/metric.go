5031093fca84908fc3c2a4191c88c4b0eaeed4ec
Vishal Kanojwar <vishal@Vishal.local>
2026-07-10 13:54:21 +0530

    feat(model): introduce metrics domain model

--- internal/model/metric.go
+++ internal/model/metric.go
@@ -1,23 +1,11 @@
 package model
 
-type Metric struct {
-	Metric        string
-	Object        Object
-	Category      Category
-	Unit          string
-	RecordingRule string
-	Dashboards    []string
-	Alerts        []string
-}
-
-package model
-
 // Metric describes a metric definition.
 type Metric struct {
-	Metric        string   `yaml:"metric"`
-	Object        Object   `yaml:"object"`
-	Category      Category `yaml:"category"`
-	Unit          string   `yaml:"unit"`
+	Metric   string   `yaml:"metric"`
+	Object   Object   `yaml:"object"`
+	Category Category `yaml:"category"`
+	Unit     string   `yaml:"unit"`
 
 	RecordingRule string   `yaml:"recording_rule"`
 	Dashboards    []string `yaml:"dashboards"`
